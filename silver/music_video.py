b27adfa3e344f6bd79b1e53cbf2619f27970c471
Ellie <dev.ellie7@gmail.com>
2022-03-11 16:23:04 +0900

    minimum capacity for m dvd

---
+++ silver/music_video.py
@@ -0,0 +1,48 @@
+
+import sys
+
+
+input = sys.stdin.readline
+
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+# print(n, m, a)
+
+# 9 3
+# 1 2 3 4 5 6 7 8 9
+def Count(mid):
+    sum = 0  
+    cnt = 1
+    for x in a:
+        if sum + x > mid:
+            cnt+=1 
+            sum = x
+        else : 
+            sum += x
+            
+    return cnt
+
+        
+lt = 1
+maxi = 0
+for x in a:
+    maxi += x
+rt = maxi
+
+
+
+res = 0
+while lt <= rt:    
+    mid = (lt + rt)//2
+    # 최소 m개여야 한다. 
+    if Count(mid) <= m :
+        res = mid
+        rt = mid -1
+    elif Count(mid)>m:
+        lt = mid + 1
+
+print(res)
+
+
