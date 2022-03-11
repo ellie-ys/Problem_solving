2969cc153b0cfd5b99bd0e8954539efc42949e57
Ellie <dev.ellie7@gmail.com>
2022-03-11 11:41:59 +0900

    binary search using mid variable

--- silver/search.py
+++ silver/search.py
@@ -8,8 +8,22 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 a.sort()
-# print(a)
-print(a.index(m)+1)
+
+# index찾는 파이썬 내장함수 사용 
+# print(a.index(m)+1)
+lt = 0
+rt = n
+for _ in range(n):
+    mid = (lt+rt)//2
+
+    if a[mid] > m:
+        rt = mid -1
+    elif a[mid] < m:
+        lt = mid + 1 
+    # else:
+    #     mid
+
+print(mid+1)
 
 # 8 32
 # 23 87 65 12 57 32 99 81
