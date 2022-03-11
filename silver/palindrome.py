1e4a5aab50f31ae277cc8c07e99edb5776901ffc
Ellie <dev.ellie7@gmail.com>
2022-03-11 10:46:31 +0900

    add column palindrome

--- silver/palindrome.py
+++ silver/palindrome.py
@@ -18,21 +18,29 @@ a = [list(map(int, input().split())) for _ in range(7)]
 # 1 3 1 3 5 3 2 
 # 1 1 2 5 6 5 2
 # 1 2 2 2 2 1 5
+
+# 세로 회문은 
+# tmp2= a[i:i+5][j]
+# 이렇게 작성해서 또 비교해주면 된다고 생각함 
+# but out of range 나옴
+# 열 slice는 되지만, 행 slice는 이런 방법으로 하지 않는 것으로 판단.
+# ->  그냥 비교해주자.
+# 
 cnt = 0 
 for i in range(3):
     for j in range(7):
             tmp = a[j][i:i+5]
-            print(tmp)
-            
+            # print(tmp)
             if tmp == tmp[::-1]:
                 cnt+=1
 
-for i in range(3):
-    for j in range(7):
-        tmp2 = a[j][i:i+5]
-        print(tmp2)
-        if tmp2 == tmp[::-1]:
-            cnt +=1
+            for k in range(2):
+                if a[i+k][j] != a[i-k+4][j]:
+                    break
+            else:    
+                cnt+=1
+
+
 
 print(cnt)
 
