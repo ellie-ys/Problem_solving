d45944d6f85d7d14fa632c3eb82ccffa68bbb5fe
Ellie <dev.ellie7@gmail.com>
2022-03-13 11:26:42 +0900

    Incomplete,  think more  about inverse function

--- silver/inverse_sequ.py
+++ silver/inverse_sequ.py
@@ -13,13 +13,28 @@ a = list(map(int,input().split()))
 expect = [0]*n
 # print(a[0], a[1])
 # 1이 들어갈 인덱스! 
+# temp = []
+# for i in range(n):
+#     temp.append(i+1)
 
-for i in range(n):
-    if expect[a[i]] == 0:
+#만약 cnt가 5라면 5인덱스에넣으면 된다.
+# 만약 expect에 0이 아니고 숫자가 들어있을경우. 다음칸으로 넘어가줌
 
-        expect[a[i]]= i+1
-    else :
-        expect[a[i]+1] = i+1
+print(expect)
+print(expect[0:5])
+
+for i in range(2):
+    level = expect[0:a[i]].count(0)
+    temp = a[i]
+    if  level == temp:
+        expect[temp] = i+1
+    elif temp - level == 1 :
+        expect[temp+1] = i+1
+    elif temp - level == 2:
+        expect[temp+2] = i+1
+    
+
+    
 
 print(expect)    
 # def Count(idx):
